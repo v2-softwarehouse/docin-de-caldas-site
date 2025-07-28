6b8706216a070d1971a6c3053b9491fefe663a06
V2 dev <v2.softwarehouse@gmail.com>
2025-07-28 15:11:49 -0300

    review deployment

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Home from './pages/Home'
 import Checkout from './pages/Checkout'
 import Catalog from './pages/Catalog'
 import Cart from './pages/Cart'
-import Checkout from './pages/Checkout'
 
 export default function App() {
   return (
@@ -12,16 +11,15 @@ export default function App() {
       <nav style={{ display: 'flex', gap: '1rem', padding: '1rem' }}>
         <Link to="/">Início</Link>
         <Link to="/catalog">Catálogo</Link>
+        <Link to="/cart">Carrinho</Link>
         <Link to="/checkout">Checkout</Link>
-<Link to="/cart">Carrinho</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/catalog" element={<Catalog />} />
-        <Route path="/checkout" element={<Checkout />} />
         <Route path="/cart" element={<Cart />} />
-  <Route path="/checkout" element={<Checkout />} />
-</Routes>
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
     </BrowserRouter>
   )
 }
